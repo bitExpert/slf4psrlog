471d849aee57fb32d65ae5762832eaf79190d85e
Stephan Hochdörfer <S.Hochdoerfer@bitExpert.de>
2015-06-07 21:07:19 +0200

    Added LoggerFactory implementation and test cases.

---
+++ src/bitExpert/Slf4PsrLog/LoggerFactory.php
@@ -0,0 +1,61 @@
+<?php
+
+/*
+ * This file is part of the Simple Logging Facade for PSR-3 Loggers package.
+ *
+ * (c) bitExpert AG
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace bitExpert\Slf4PsrLog;
+
+use Psr\Log\LoggerInterface;
+use RuntimeException;
+
+/**
+ * Factory implementation to configure and return {@link \Psr\Log\LoggerInterface} instances.
+ *
+ * @api
+ */
+class LoggerFactory
+{
+    /**
+     * @var callable
+     */
+    private static $callback = null;
+
+    /**
+     * Registers a callable as delegate to create a logger instance when calling
+     * the getLogger() method.
+     *
+     * @param callable $callback
+     */
+    public static function registerFactoryCallback($callback)
+    {
+        self::$callback = $callback;
+    }
+
+    /**
+     * Returns a configured logger instance for the given $channel. If no FactoryCallback
+     * is registered it will return an instance of {@link \bitExpert\Slf4PsrLog\NullLogger}.
+     *
+     * @param $channel
+     * @returns LoggerInterface
+     * @throws RuntimeException
+     */
+    public static function getLogger($channel)
+    {
+        $callback = self::$callback;
+        if (null === $callback) {
+            return new NullLogger();
+        }
+
+        $logger = $callback($channel);
+        if ($logger instanceof LoggerInterface) {
+            return $logger;
+        }
+
+        throw new RuntimeException('Callback did not return an instance of \Psr\Log\LoggerInterface!');
+    }
+}
---
+++ tests/bitExpert/Slf4PsrLog/LoggerFactoryUnitTest.php
@@ -0,0 +1,73 @@
+<?php
+
+/*
+ * This file is part of the Simple Logging Facade for PSR-3 Loggers package.
+ *
+ * (c) bitExpert AG
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace bitExpert\Slf4PsrLog;
+
+/**
+ * Unit test for {@link \bitExpert\Slf4PsrLog\LoggerFactory}.
+ *
+ * @covers \bitExpert\Slf4PsrLog\LoggerFactory
+ */
+class LoggerFactoryUnitTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function returnsNullLoggerWhenCallingWithoutConfiguredCallback()
+    {
+        $logger = LoggerFactory::getLogger('channel');
+
+        $this->assertInstanceOf(NullLogger::class, $logger);
+    }
+
+    /**
+     * @test
+     * @expectedException \RuntimeException
+     */
+    public function throwsRuntimeExceptionWhenCallableDoesNotReturnLoggerInstance()
+    {
+        LoggerFactory::registerFactoryCallback(function($channel) {
+            return null;
+        });
+
+        LoggerFactory::getLogger('channel');
+    }
+
+    /**
+     * @test
+     */
+    public function getLoggerCallIsDelegatedToCallable()
+    {
+        $loggerMock = $this->getMock('\Psr\Log\LoggerInterface');
+        LoggerFactory::registerFactoryCallback(function($channel) use ($loggerMock) {
+           return $loggerMock;
+        });
+
+        $logger = LoggerFactory::getLogger('test');
+
+        $this->assertSame($loggerMock, $logger);
+    }
+
+    /**
+     * @test
+     */
+    public function channelParamIsPassedToCallable()
+    {
+        $loggerMock = $this->getMock('\Psr\Log\LoggerInterface');
+        LoggerFactory::registerFactoryCallback(function($channel) use ($loggerMock) {
+            $loggerMock->channel = $channel;
+            return $loggerMock;
+        });
+
+        $logger = LoggerFactory::getLogger('test');
+
+        $this->assertSame('test', $logger->channel);
+    }
+}
